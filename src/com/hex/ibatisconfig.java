25bc9ab9dac558b73bef228fa1ee7d86086c06b0
huoex <huoex0628@gmai.com>
2014-05-19 20:34:34 +0800

    SI First
    Signed-off-by: huoex <huoex0628@gmai.com>

---
+++ src/com/hex/ibatisconfig.java
@@ -0,0 +1,32 @@
+package com.hex;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+public class ibatisconfig {
+
+	  /**
+	   * SqlMapClient instances are thread safe, so you only need one.
+	   * In this case, we'll use a static singleton.  So sue me.  ;-)
+	   */
+	  private static SqlMapClient sqlMapper;
+
+	  /**
+	   * It's not a good idea to put code that can fail in a class initializer,
+	   * but for sake of argument, here's how you configure an SQL Map.
+	   */
+	  static {
+	    try {
+	      Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+	      sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+	      reader.close(); 
+	    } catch (IOException e) {
+	      // Fail fast.
+	      throw new RuntimeException("Something bad happened while building the SqlMapClient instance." + e, e);
+	    }
+	  }
+}
---
+++ src/com/hex/ibatisconfigtest.java
@@ -0,0 +1,15 @@
+package com.hex;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ibatisconfigtest {
+
+	@Test
+	public void test() {
+		ibatisconfig ic =  new ibatisconfig();
+//		fail("Not yet implemented");
+	}
+
+}
