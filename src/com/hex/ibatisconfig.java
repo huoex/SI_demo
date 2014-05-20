a0f5785ddcd98088554d5f2ad548fb09b5e05182
huoex <huoex0628@gmai.com>
2014-05-20 14:43:18 +0800

    select OK

--- src/com/hex/ibatisconfig.java
+++ src/com/hex/ibatisconfig.java
@@ -2,6 +2,8 @@ package com.hex;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.sql.SQLException;
+import java.util.List;
 
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
@@ -9,24 +11,30 @@ import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 
 public class ibatisconfig {
 
-	  /**
-	   * SqlMapClient instances are thread safe, so you only need one.
-	   * In this case, we'll use a static singleton.  So sue me.  ;-)
-	   */
-	  private static SqlMapClient sqlMapper;
+	/**
+	 * SqlMapClient instances are thread safe, so you only need one. In this
+	 * case, we'll use a static singleton. So sue me. ;-)
+	 */
+	private static SqlMapClient sqlMapper;
 
-	  /**
-	   * It's not a good idea to put code that can fail in a class initializer,
-	   * but for sake of argument, here's how you configure an SQL Map.
-	   */
-	  static {
-	    try {
-	      Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
-	      sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
-	      reader.close(); 
-	    } catch (IOException e) {
-	      // Fail fast.
-	      throw new RuntimeException("Something bad happened while building the SqlMapClient instance." + e, e);
-	    }
-	  }
+	/**
+	 * It's not a good idea to put code that can fail in a class initializer,
+	 * but for sake of argument, here's how you configure an SQL Map.
+	 */
+	static {
+		try {
+			Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+			sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+			reader.close();
+		} catch (IOException e) {
+			// Fail fast.
+			throw new RuntimeException(
+					"Something bad happened while building the SqlMapClient instance."
+							+ e, e);
+		}
+	}
+
+	public static List selectAllAccounts() throws SQLException {
+		return sqlMapper.queryForList("selectAllAccounts");
+	}
 }
--- src/com/hex/ibatisconfigtest.java
+++ src/com/hex/ibatisconfigtest.java
@@ -1,6 +1,8 @@
 package com.hex;
 
-import static org.junit.Assert.*;
+import java.awt.List;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -9,6 +11,14 @@ public class ibatisconfigtest {
 	@Test
 	public void test() {
 		ibatisconfig ic =  new ibatisconfig();
+		try {
+			ArrayList al = (ArrayList) ic.selectAllAccounts();
+			ic.selectAllAccounts();
+			System.out.println();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// TODO
 //		fail("Not yet implemented");
 	}
