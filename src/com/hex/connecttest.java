99224e2fdd9a70225a9235dfc2619148dfce3bc4
huoex <huoex0628@gmai.com>
2014-05-26 11:27:03 +0800

    ibatis + spring first

---
+++ src/com/hex/connecttest.java
@@ -0,0 +1,24 @@
+package com.hex;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+public class connecttest {
+
+	@Test
+	public void test() throws SQLException {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		SqlMapClient sql =(SqlMapClient) ac.getBean("sqlMapClient");
+		List ls = sql.queryForList("selectAllAccounts");
+		
+		System.out.println("");
+//		fail("Not yet implemented");
+	}
+
+}
